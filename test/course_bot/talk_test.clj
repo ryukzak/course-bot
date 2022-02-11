89298f9e7fd2033cc9c55fb5f7e61d89538b4c2c
Aleksandr Penskoi <aleksandr.penskoi@gmail.com>
2022-02-11 14:47:00 +0300

    Fix test.

--- test/course_bot/talk_test.clj
+++ test/course_bot/talk_test.clj
@@ -1,13 +1,13 @@
 (ns course-bot.talk-test
   (:require [clojure.test :refer :all]
-            [course-bot.talk :refer :all]
+            [course-bot.talk :as talk]
             [codax.core :as c]))
 
 (deftest test-talk-return-value
   (let [test-db (c/open-database! "talk-test-codax")
-        test-talk1 (talk test-db "cmd" :start (fn [tx _msg] (wait tx)))
-        test-talk2 (talk test-db "cmd" :start (fn [_tx _msg] :ok))
-        test-talk3 (talk test-db "cmd" :start (fn [_tx _msg] nil))]
+        test-talk1 (talk/def-talk test-db "cmd" :start (fn [tx _msg] (talk/wait tx)))
+        test-talk2 (talk/def-talk test-db "cmd" :start (fn [_tx _msg] :ok))
+        test-talk3 (talk/def-talk test-db "cmd" :start (fn [_tx _msg] nil))]
     (is (= :ok (test-talk1 {:message {:text "/cmd"}})))
     (is (= nil (test-talk1 {:message {:text "bla-bla"}})))
     ;; handler should return nil value to pass
