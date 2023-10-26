789cdc9feb105c0ef1e184588c1c916756ac743e
Aleksandr Penskoi <aleksandr.penskoi@gmail.com>
2023-10-26 15:53:25 +0200

    Rewrite course-bot.plagiarism:
    - Wrap forest in plagiarism-db map.
    - Move consimilo settings plagiarism-db.
    - Redesign API to work with any texts.
    - Store original text in documents.

--- src/course_bot/plagiarism.clj
+++ src/course_bot/plagiarism.clj
@@ -1,51 +1,85 @@
 (ns course-bot.plagiarism
-  (:require [consimilo.core :as consimilo])
-  (:require [codax.core :as codax]))
+  (:require [clojure.java.io :as io]
+            [consimilo.core :as consimilo]
+            [course-bot.plagiarism :as plagiarism])
+  (:require [course-bot.general :as general :refer [tr]]))
 
-(def top-k 7)
-(def cosine-threshold 25.00)
+(general/add-dict
+ {:en
+  {:plagiarism
+   {:forest-failure  "I failed to reach forest file, my Lord!"}}
+  :ru
+  {:plagiarism
+   {:forest-failure  "Не удалось подключиться к хэш-лесу, мой господин!"}}})
 
-(defn add-to-forest
-  "Add new entry to lsh-forest"
-  [forest essay-text essay-code id]
-  (consimilo/add-strings-to-forest
-   [{:id (str essay-code id) :features essay-text}]
-   :forest forest))
+(def default-conf {:top-k 10
+                   :cosine-threshold 25.00
+                   :sim-fn :cosine})
 
-(defn get-test-forest
-  [path]
-  (try
-    (consimilo/thaw-forest path)
-    (catch Exception _e
-      (consimilo/add-strings-to-forest
-       [{:id "test-id" :features "test id text"}]))))
+(comment 'plagiarism-db
+         (assoc default-conf
+                :forest-filename "tmp/forest/data2"
+                :forest 'CONSIMILO-FOREST
+                :texts-path "tmp/texts"))
 
-(defn get-vector-cosine
-  "Returns the cosine between two most similar vectors
-  in existing forest"
-  [forest essay-text]
-  (let [similar-vectors (consimilo/similarity-k
-                         forest
-                         top-k
-                         essay-text
-                         :sim-fn :cosine)]
-    (apply min-key val similar-vectors)))
+(defn open-path-or-fail [path]
+  (let [forest-filename (str path "/forest")
+        texts-path (str path "/texts")
+        db (assoc default-conf
+                  :forest-filename forest-filename
+                  :texts-path texts-path)]
+    (try
+      (assoc db :forest (consimilo/thaw-forest forest-filename))
+      (catch java.io.FileNotFoundException _
+        (println "Forest file not found, creating new one...")
+        (let [forest (consimilo/add-strings-to-forest [])]
+          (io/make-parents forest-filename)
+          (consimilo/freeze-forest forest forest-filename)
+          (assoc db :forest forest)))
+      (catch Exception e
+        (println (tr :forest-failure))
+        (println e)
+        (System/exit 1)))))
 
-(defn print-plagiarise-log
-  [forest-key cosine plag-essay-text plag-id plag-code tx]
-  (let [code-list (split-at 6 forest-key)
-        essay-code (apply str (first code-list))
-        id (apply str (second code-list))
-        essay-text (codax/get-at tx [id :essays essay-code :text])]
-    (println
-     (format "Essay with code '%s' submitted by '%s' was plagiarised from '%s', essay code: '%s' with cosine %s.\nOriginal essay text:\n%s\nPlagiarised essay text:\n%s",
-             plag-code, plag-id, id, essay-code, cosine, essay-text, plag-essay-text))
-    true))
+(defn register-text!
+  "Add new entry to lsh-forest.
 
-(defn is-essay-plagiarised
-  [forest essay-text id essay-code tx]
-  (let [minimal-vector
-        (get-vector-cosine forest essay-text)]
-    (if (< (val minimal-vector) cosine-threshold)
-      (print-plagiarise-log (name (key minimal-vector)) (val minimal-vector) essay-text id essay-code tx)
-      false)))
+  key should be a string to be consistent with in
+  input and output and valid as a filename."
+  ([{forest-filename :forest-filename forest :forest texts-path :texts-path :as plagiarism-db} key text]
+   (consimilo/add-strings-to-forest [{:id key :features text}] :forest forest)
+   (when forest-filename
+     (consimilo/freeze-forest forest forest-filename))
+   (when texts-path
+     (let [filename (str texts-path "/" key ".txt")]
+       (io/make-parents filename)
+       (spit filename text)))
+   plagiarism-db)
+  ([forest essay-text essay-code id] ; FIXME: remove this
+   (consimilo/add-strings-to-forest
+    [{:id (str essay-code id) :features essay-text}]
+    :forest forest)))
+
+(defn similars [{forest :forest top-k :top-k sim-fn :sim-fn} text]
+  (->> (consimilo/similarity-k forest top-k text :sim-fn sim-fn)
+       (map (fn [[key similarity]]
+              {:key (name key)
+               :similarity (if (.equals ##NaN similarity)
+                             0 ;; for some reason NaN is returned sometimes.
+                             similarity)}))))
+
+(defn sorted-similars [plagiarism-db text]
+  (->> (similars plagiarism-db text)
+       (sort-by :similarity)))
+
+(defn find-original [{cosine-threshold :cosine-threshold :as plagiarism-db} text]
+  (let [similars (->> (similars plagiarism-db text)
+                      (filter #(< (:similarity %) cosine-threshold)))]
+    (when (not (empty? similars))
+      (apply min-key :similarity similars))))
+
+(defn get-text [{texts-path :texts-path} key]
+  (when texts-path
+    (let [filename (str texts-path "/" key ".txt")]
+      (when (io/file filename)
+        (slurp filename)))))
