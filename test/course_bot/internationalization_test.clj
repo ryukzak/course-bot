fbe3c0ee819a01b5160bc1b1561656f08b61891f
sasaovch <sasaovch@gmail.com>
2023-12-01 02:19:14 +0300

    Added test

--- test/course_bot/internationalization_test.clj
+++ test/course_bot/internationalization_test.clj
@@ -9,7 +9,14 @@
       "yes" (normalize-yes-no-text "YeS")
       "no" (normalize-yes-no-text "no")
       "no" (normalize-yes-no-text "NO")
-      "some input" (normalize-yes-no-text "some input")
+      "some input" (normalize-yes-no-text "some input"))))
+
+(deftest test-normalize-yes-no-text-ru
+  (testing
+   (i18n/set-locales [:ru :en])
+    (are [expected actual] (= expected actual)
       "yes" (normalize-yes-no-text "ДА")
       "no" (normalize-yes-no-text "нет")
-      "no" (normalize-yes-no-text "НЕТ"))))
+      "no" (normalize-yes-no-text "НЕТ")
+      "что-нибудь" (normalize-yes-no-text "что-нибудь"))
+    (i18n/set-locales [:en])))
