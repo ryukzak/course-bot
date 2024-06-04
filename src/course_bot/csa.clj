21c2a82ae342935550c7eab53315cb1e29b8e09f
Aleksandr Penskoi <aleksandr.penskoi@gmail.com>
2024-06-04 12:36:56 +0200

    Disable lab1 feedback talk.

--- src/course_bot/csa.clj
+++ src/course_bot/csa.clj
@@ -79,7 +79,7 @@
       (pres/submit-talk db conf "lab1")
       (pres/schedule-talk db conf "lab1")
       (pres/soon-talk db conf "lab1")
-      (pres/feedback-talk db conf "lab1")
+      ;; (pres/feedback-talk db conf "lab1")
       (pres/lessonstat-talk db conf "lab1")
 
       (pres/agenda-talk db conf "lab1")
